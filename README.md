fc26fc7b11db2f0b74c66de5765c7548d4b7d9c8
Nureddin Cüneyd ERDOĞAN <nureddincuneyd@gmail.com>
2021-10-30 04:23:18 +0300

    Get Cryptocurrent price command
    add forex_python, cryptocompare library and get price command

--- README.md
+++ README.md
@@ -5,21 +5,49 @@
 > Reauired Python Version (Python 3.9.7)
 
 
+
+
 ### Necessary Install
 
  - :white_check_mark: Python3
  - :white_check_mark: Telethon Lib.
  - :white_check_mark: Your Telegram Api (Api-Id, Api-Hash, Channel/Group Link)
+ - :white_check_mark: cryptocompare Sign-Up
+ - :white_check_mark: forex_python
 
 
 
-### Install Telegram Lib. Ternm (Terminal/CMD)
+### Install Library
 
 [Telethon](https://docs.telethon.dev/en/latest/basic/installation.html) WEB Site.
 
 ```
-python3 -m pip install --upgrade pip <br/>
+python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade telethon
 ```
 
+[forex_python](https://forex-python.readthedocs.io/en/latest/installation.html) WEB Site.
 
+```
+pip install forex-python
+```
+
+[CryptoCompare](https://pypi.org/project/cryptocompare/) WEB Site.
+
+```
+pip install cryptocompare
+```
+
+
+
+
+###
+
+### SignUp to CryptoCompare
+
+[CryptoCompare](https://www.cryptocompare.com/)
+
+Get api key, paste to THIS LINE:
+```
+cryp.cryptocompare._set_api_key_parameter('YOUR_API_KEY')
+```
--- main.py
+++ main.py
@@ -1,26 +1,45 @@
-from telethon import TelegramClient, events, sync
-import time
-import configparser
+try:
+    import configparser
+    from forex_python.converter import CurrencyRates
+    import cryptocompare as cryp
+except:
+    print("Python forex_python, configparser or cryptocompare library not installed!")
+
+try:
+    from telethon import TelegramClient, events, sync
+    from telethon.tl.functions.messages import ImportChatInviteRequest
+    import asyncio
+except:
+    print("Telethon not installed!")
 
 parser = configparser.ConfigParser()
 parser.read("config.txt")
 
+cryp.cryptocompare._set_api_key_parameter('YOUR_API_KEY')
+currentRate = CurrencyRates()
+
 print("Telegram API Defined")
 api_id = parser.get("config", "TelApi_id")
 api_hash = parser.get("config", "TelApi_hash")
-telChnlURL = parser.get("config", "TelChnlLink")
-
+#telChnlURL = parser.get("config", "TelChnlLink")
 
 
 client = TelegramClient('anon', api_id, api_hash)
+client.start()
+
 
 @client.on(events.NewMessage)
 async def my_event_handler(event):
-    if 'hello' in event.raw_text:
-        await event.reply('hi!')
+    if '!USD/TRY' in event.raw_text:
+        await event.reply(str(currentRate.get_rate('USD','TRY')))
+    if '!BTC' in event.raw_text:
+        await event.reply(str(cryp.get_price('BTC', currency='USD')))
+    if '!ETH' in event.raw_text:
+        await event.reply(str(cryp.get_price('ETH', currency='USD')))
+    if '!COMP' in event.raw_text:
+        await event.reply(str(cryp.get_price('COMP', currency='USD')))
+    
 
-client.start()
+
 client.run_until_disconnected()
-
-# Terminal Output {          Signed in successfully as [TelegramUserName]            }
 
